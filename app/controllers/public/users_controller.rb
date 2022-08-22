a999c3e59a121ce6d4bbb5119de81405d76c57be
moneeeet <t.o.p11040809@gmail.com>
2022-08-22 04:03:41 +0000

    update

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -35,6 +35,7 @@ class Public::UsersController < ApplicationController
     @user = User.find(params[:id])
     favorites= Favorite.where(user_id: @user.id).pluck(:post_id)
     @favorite_posts = Post.find(favorites)
+
   end
 
   def unsubscribe
