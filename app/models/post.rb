24fd84eb8223d097666e8a5e0249c855e8bac8c9
moneeeet <t.o.p11040809@gmail.com>
2022-08-28 16:17:18 +0000

    [add] reportバリデーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,8 +12,8 @@ class Post < ApplicationRecord
 
   has_one_attached :image
 
-  validates :title, presence:true,length:{maximum:20}
-  validates :body, presence:true,length:{maximum:200}
+  validates :title, presence:true,length:{maximum:20, minimum:3}
+  validates :body, presence:true,length:{maximum:200, minimum:10}
 
 
 
