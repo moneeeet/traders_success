fdb7667db70939178abe9525d77a2d9bb626d319
moneeeet <t.o.p11040809@gmail.com>
2022-08-29 13:19:42 +0000

    Edit

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,7 +2,7 @@ class Report < ApplicationRecord
   belongs_to :reporter, class_name: "User"
   belongs_to :reported, class_name: "User"
 
-  #validates :reason, presence:true,length:{maximum:150, minimum:10}
+  validates :reason, presence:true,length:{maximum:150, minimum:2}
 
   enum status: { waiting: 0, keep: 1, finish: 2 }
 end
